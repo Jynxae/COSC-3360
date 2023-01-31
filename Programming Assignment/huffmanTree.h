76dcf82a0a0b259ecbe01af26d9aee7da27312ee
Jynxae <60587439+Jynxae@users.noreply.github.com>
2023-01-30 23:59:39 -0600

    Create huffmanTree.h

---
+++ Programming Assignment/huffmanTree.h
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+struct node{
+    char let;
+    int frequency;
+    string code;
+    node* right;
+    node* left;
+    node* next;
+    node(char chara, int freq)
+    {
+        let = chara;
+        frequency = freq;
+        left = right = nullptr;
+    }
+};
+
+class huffmanTree{
+private:
+    node* tree;
+public:
+    huffmanTree()
+    {
+        tree = nullptr;
+    }
+    node* codeHuffmanTree(unordered_map<char,int>);
+    void print(node*,string);
+};
