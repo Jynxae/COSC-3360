2da1710381ab483d2bcd34462c961321b2ff2d9f
Jynxae <60587439+Jynxae@users.noreply.github.com>
2023-02-22 12:46:28 -0600

    Update main.cpp

--- Programming Assignment/main.cpp
+++ Programming Assignment/main.cpp
@@ -1,39 +1,40 @@
-#include<iostream>
-#include <pthread.h>
-#include<fstream>
-#include<sstream>
+#include <iostream>
+#include <pthread.h>    //needed to do threading
+#include <fstream>
+#include <sstream>  //string strean
 #include <vector>
 #include "huffmanTree.cpp"
 #include <unordered_map>
 using namespace std;
 
-struct ThreadArgs{
-    node* root;
-    string binaryCode;          //found out how to pass multiple arguments into a thread function like this from stack overflow and https://w3.cs.jmu.edu/
+struct ThreadArgs
+{
+    node *root;
+    string binaryCode; // found out how to pass multiple arguments into a thread function like this from stack overflow and https://w3.cs.jmu.edu/
     vector<int> position;
-    vector<char> message;       //changed to vector, messy to deal with as string. can just print out indexes with no spaces
+    vector<char> message; // changed to vector, messy to deal with as string. can just print out indexes with no spaces
 };
 
-void* threadFunction(void* args) //here is where we actually decode
+void *threadFunction(void *args) // here is where we actually decode, referred to https://iq.opengenus.org/ with best way of traversing
 {
-    ThreadArgs* arg = (ThreadArgs*) args;
+    ThreadArgs *arg = (ThreadArgs *)args;
 
-    node* curr = arg->root;
-    for (int x = 0; x < arg->binaryCode.length(); x++)  //traverse the BINARY SYMBOL string, character by character.
+    node *curr = arg->root;
+    for (int x = 0; x < arg->binaryCode.length(); x++) // traverse the BINARY SYMBOL string, character by character.
     {
-        if (arg->binaryCode[x] == '0')
+        if (arg->binaryCode[x] == '0')  //think like we are doing a huffman tree by hand, we go left when zero
         {
             curr = curr->left;
         }
-        else if (arg->binaryCode[x] == '1')
+        else if (arg->binaryCode[x] == '1') //and we go right when one, stop once we reach the wanted node, in our case, once we finish reading the string character by character
         {
             curr = curr->right;
         }
     }
 
-    char letterResult = curr->let;  //gives me the final letter of the node i landed on from above^^
+    char letterResult = curr->let; // gives me the final letter of the node i landed on from above^^
 
-    for(int x = 0; x < arg->position.size(); x++)
+    for (int x = 0; x < arg->position.size(); x++)
     {
         arg->message.at(arg->position.at(x)) = letterResult;
     }
@@ -43,75 +44,79 @@ void* threadFunction(void* args) //here is where we actually decode
 int main(int argc, char *argv[])
 {
     string inputFile, compressedFile, line;
-    // cin >> inputFile;
-    // cin >> compressedFile;
+    //  cin >> inputFile;
+    //  cin >> compressedFile;
 
-    ifstream fin("input1");
-    ifstream cfin("cInput1");
-    char let;
-    int num, freq;
+    ifstream fin("input2");
+    ifstream cfin("cInput2");
+    
     int mSize = 0;
-    string binary = ""; 
-    unordered_map<char,int> input;
+    int argSize = 0;
+    string binary = "";
+    unordered_map<char, int> input;
     unordered_map<int, vector<int>> code;
-    static vector<pthread_t> th (mSize);    //remove static if it messes with code possibly
-    static vector<ThreadArgs*> passer;
+    static vector<pthread_t> th(mSize); // remove static if it messes with code possibly
+    static vector<ThreadArgs *> passer;
 
-    while(getline(fin,line))
+    while (getline(fin, line))
     {
+        char let;
+        int num;
         let = line[0];
-        num = int(line[2]) - 48;
+        num = int(line[2]) - '0';    //or we can do - 48, the ascii value of zero
         input[let] = num;
-        mSize += line[2] - 48; //plus the frequency
+        mSize += line[2] - '0'; // plus the frequency
     }
 
     huffmanTree call;
-    vector<char> message (mSize);       //DECLARE VECTOR HERE INSTEAD AND KEEP MSIZE UP TOP. Since im still adjusting size. Do not give size to the vector until its the final size
-    node * root = call.codeHuffmanTree(input);
+    vector<char> message(mSize); // DECLARE VECTOR HERE INSTEAD AND KEEP MSIZE UP TOP. Since im still adjusting size. Do not give size to the vector until its the final size
+    node *root = call.codeHuffmanTree(input);
     call.print(root, "");
-    cout << endl;
 
-    while(getline(cfin,line))
+    while (getline(cfin, line))
     {
-        pthread_t thread1;      //decalring a new thread each time i get a line
-        ThreadArgs* argument = new ThreadArgs();
+        pthread_t thread1; // decalring a new thread each time i get a line
+        ThreadArgs *argument = new ThreadArgs();
         argument->root = root;
         argument->message = message;
-        stringstream str(line);     //stringstream allows me to read in individual values after i getline, REMEMBER TO USE IN FUTURE ASSIGNMENTS!!!!!
+        stringstream str(line); // stringstream allows me to read in individual values after i getline, REMEMBER TO USE IN FUTURE ASSIGNMENTS!!!!!
 
-        str >> binary;  //after i read in a whole line, str allows me to read one at a time.
+        str >> binary; // after i read in a whole line, str allows me to read one at a time.
         vector<int> spot;
         int value;
-        while(str >> value) {
+        argSize++;
+        while (str >> value)
+        {
             spot.push_back(value);
         }
 
         argument->binaryCode = binary;
         argument->position = spot;
 
-        pthread_create(&thread1, NULL, threadFunction, (void*) argument);
-        passer.push_back(argument);     
+        pthread_create(&thread1, NULL, threadFunction, (void *)argument); // remember for exam to DEREFERENCE THE THREAD!!!! must also cast the argument as void*
+        passer.push_back(argument);
         th.push_back(thread1);
     }
 
-    for(int x = 0; x < th.size(); x++){     //referenced from rincons code
-            pthread_join(th.at(x),NULL);
+    for (int x = 0; x < th.size(); x++)
+    { // referenced from rincons code
+        pthread_join(th.at(x), NULL);
     }
 
+    // add another for loop here for something??
 
-
-    //add another for loop here for something??
-    // for(int arg = 0; x < passer.size(); arg++)
-    // {
-    //     for(int spot = 0; spot < arg->position.size(); spot++)
-    //     {
-    //         message.at(spot) = arg->message.at(spot);
-    //     }
-    // }
-
-    for(auto arg : passer)      //basically arg takes the value of the first value of passer and when it loops again, it takes the next value
+    for(int x = 0; x < passer.size(); x++)
     {
-        for(auto spot : arg->position)
+        vector<int> pos = passer.at(x)->position;
+        for(int i = 0; i < mSize; i++)
+        {
+            message.at(i) = passer.at(x)->message.at(i);
+        }
+    }
+
+    for (auto arg : passer) // basically arg takes the value of the first value of passer and when it loops again, it takes the next value
+    {
+        for (auto spot : arg->position)
         {
             message.at(spot) = arg->message.at(spot);
         }
@@ -120,12 +125,12 @@ int main(int argc, char *argv[])
     cout << "Original message: ";
 
     string decoded = "";
-    for(int x = 0; x < message.size(); x++)
+    for (int x = 0; x < message.size(); x++)
     {
-        decoded += message.at(x);   //doesnt output in terminal while debugging if i cout from here??
+        decoded += message.at(x); // doesnt output in terminal while debugging if i cout from here??
     }
 
-    cout << decoded;    //YAYAYYA
-    
+    cout << decoded; // YAYAYYA
+
     return 0;
 }
